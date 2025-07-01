6086d14e512b62c124dd41062ea8b74d6f7da43b
iceappletea <vita244631@gmail.com>
2025-07-01 17:41:09 +0800

    Initial commit: upload C#simulation-peak

---
+++ 演示05.移動相位或位置模擬/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.formsPlot2 = new ScottPlot.WinForms.FormsPlot();
+            this.formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.hScrollBar2);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.hScrollBar1);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(1902, 1033);
+            this.splitContainer1.SplitterDistance = 720;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.formsPlot2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.formsPlot1);
+            this.splitContainer2.Size = new System.Drawing.Size(1902, 720);
+            this.splitContainer2.SplitterDistance = 720;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // formsPlot2
+            // 
+            this.formsPlot2.DisplayScale = 0F;
+            this.formsPlot2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formsPlot2.Location = new System.Drawing.Point(0, 0);
+            this.formsPlot2.Name = "formsPlot2";
+            this.formsPlot2.Size = new System.Drawing.Size(720, 720);
+            this.formsPlot2.TabIndex = 2;
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.DisplayScale = 0F;
+            this.formsPlot1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formsPlot1.Location = new System.Drawing.Point(0, 0);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(1178, 720);
+            this.formsPlot1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(428, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 28);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "MaxY2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(12, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 28);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "nh";
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.LargeChange = 1;
+            this.hScrollBar2.Location = new System.Drawing.Point(250, 189);
+            this.hScrollBar2.Maximum = 10;
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(1500, 17);
+            this.hScrollBar2.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(12, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 28);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "0.0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(1440, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 28);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "MaxY3";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(1799, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 41);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "End";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 1;
+            this.hScrollBar1.Location = new System.Drawing.Point(250, 125);
+            this.hScrollBar1.Maximum = 360;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(1500, 17);
+            this.hScrollBar1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(1799, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 41);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Button button1;
+        private ScottPlot.WinForms.FormsPlot formsPlot2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 演示05.移動相位或位置模擬/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ScottPlot;
+using ScottPlot.Plottables;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        //private FormsPlot formsPlot1; // Declare the FormsPlot control  
+        double[] ts;
+        double[] ys1, ys2, ys3, ysTotal;
+        double[] ys21, ys22, ysTotal2;
+        double wt, eps;
+        int freq = 400;
+        int dt = 0;
+        double lambda = 0.85; // Wavelength in meters
+        double h = 0.01; // Distance between the sources in meters
+        double nh;
+        double k; // Wave number (2 * pi / lambda)
+
+        double r1, r2, r3; // Distances from the sources to the point of observation    
+        double r21, r22;
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit(); // Close the application 
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // Create a new plot    
+            //var plt = formsPlot1.Plot;  
+
+            // Generate X-axis data    
+            ts = Generate.Range(0, 10, 0.1);
+            ys1 = new double[ts.Length];
+            ys2 = new double[ts.Length];
+            ys3 = new double[ts.Length];
+            ysTotal = new double[ts.Length];
+
+            ys21 = new double[ts.Length];
+            ys22 = new double[ts.Length];
+            ysTotal2 = new double[ts.Length];
+
+            timer1.Interval = 300; // Set the timer interval to 1 second (1000 milliseconds)      
+            timer1.Enabled = true; // Start the timer  
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            k = 2 * Math.PI / lambda; // Wave number
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            formsPlot1.Plot.Clear(); // Clear the previous plot data
+            formsPlot2.Plot.Clear();
+            eps = Math.PI * hScrollBar1.Value / 180.0; // Update the scrollbar value to reflect the current time step  
+            wt = 2.0 * Math.PI * freq * dt / 20000.0;
+
+            nh = hScrollBar2.Value * h; // Convert the scrollbar value to meters
+            label3.Text = "Distance: " + nh.ToString("F2") + " m"; // Display the current distance in label3
+            //Console.WriteLine($"nh = {nh} m");
+            r1 = Math.Sqrt(Math.Pow(0 - 0, 2) + Math.Pow(2 - nh, 2));
+            r2 = Math.Sqrt(Math.Pow(-Math.Sqrt(3) - 0, 2) + Math.Pow(-1 - nh, 2));
+            r3 = Math.Sqrt(Math.Pow(Math.Sqrt(3) - 0, 2) + Math.Pow(-1 - nh, 2));
+            //Console.WriteLine($"r1 = {r1}, r2 = {r2}, r3 = {r3}");
+            r21 = Math.Sqrt(Math.Pow(-2 - nh, 2) + Math.Pow(0 - 0, 2));
+            r22 = Math.Sqrt(Math.Pow(2 - nh, 2) + Math.Pow(0 - 0, 2));
+
+            for (int i = 0; i < ts.Length; i++)
+            {
+                ys1[i] = Math.Cos(k*r1 + ts[i] - wt);
+                ys2[i] = Math.Cos(k*r2 + ts[i] - wt + 2.0*Math.PI/3.0);
+                ys3[i] = Math.Cos(k*r3 + ts[i] - wt + 4.0 * Math.PI/3.0 + eps);
+                ysTotal[i] = ys1[i] + ys2[i] + ys3[i];
+
+                ys21[i] = Math.Cos(k * r21 + ts[i] - wt);
+                ys22[i] = Math.Cos(k * r22 + ts[i] - wt + Math.PI + eps);
+                ysTotal2[i] = ys21[i] + ys22[i];
+                //Console.WriteLine($"ts[{i}] = {ts[i]}, ys1[{i}] = {ys1[i]}, ys2[{i}] = {ys2[i]}, ys3[{i}] = {ys3[i]}, ysTotal[{i}] = {ysTotal[i]}");
+            }
+
+            label2.Text = "Shift: " + hScrollBar1.Value.ToString("F2"); // Display the current value of the scrollbar in label2
+
+            label1.Text =  "3 waves Interference Peak: "+ysTotal.Max().ToString("F2");
+            label4.Text = "2 waves Interference Peak: " + ysTotal2.Max().ToString("F2");
+            // Add data to the plot    
+            formsPlot1.Plot.Add.Scatter(ts, ys1);
+            formsPlot1.Plot.Add.Scatter(ts, ys2);
+            formsPlot1.Plot.Add.Scatter(ts, ys3);
+            formsPlot1.Plot.Add.Scatter(ts, ysTotal);
+
+            var sp1 = formsPlot2.Plot.Add.Scatter(ts, ys21);
+            var sp2 = formsPlot2.Plot.Add.Scatter(ts, ys22);
+            var sp3 = formsPlot2.Plot.Add.Scatter(ts, ysTotal2);
+            sp3.Color = ScottPlot.Colors.Red;
+
+            // Set plot title and axis labels
+            formsPlot1.Plot.Title("Three-Waves Interferences");
+            formsPlot1.Plot.XLabel("Time");
+            formsPlot1.Plot.YLabel("Amplitude");
+
+            formsPlot2.Plot.Title("Two-Waves Interferences");
+            formsPlot2.Plot.XLabel("Time");
+            formsPlot2.Plot.YLabel("Amplitude");
+
+            formsPlot1.Plot.Axes.SetLimits(0, 10, -3, 3);
+            formsPlot2.Plot.Axes.SetLimits(0, 10, -3, 3);
+
+            // Show legend    
+            formsPlot1.Plot.ShowLegend();
+            formsPlot2.Plot.ShowLegend();
+
+            // Refresh the plot to display changes
+            formsPlot1.Refresh();
+            formsPlot2.Refresh();
+
+            dt++; // Increment the time step for the next iteration
+        }
+    }
+}
